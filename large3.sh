b20b407dd34281809956817ff6800a55cb5c496b
Amithavinod <amithavinod2005@gmail.com>
2026-04-07 15:52:50 +0530

    Add files via upload

---
+++ large3.sh
@@ -0,0 +1,16 @@
+#read and find of 3 largest
+echo "enter the numbers"
+read n1 n2 n3
+if [ $n1 -gt $n2 ]
+then
+max=$n1
+else
+max=$n2
+fi
+if [ $n3 -gt $max ]
+then
+max=$n3
+fi
+echo "largest is $max"
+exit 0
+
