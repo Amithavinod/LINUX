8485139d595ab5779a6ba8db821bcd580cd74663
Amithavinod <amithavinod2005@gmail.com>
2026-04-16 15:16:30 +0530

    program 13

---
+++ time.sh
@@ -0,0 +1,18 @@
+if [ $# -ne 0 ]
+then
+echo "syntax is $0"
+exit 1
+fi
+t=`date +%H`
+if [ $t -ge 0 -a $t -lt 12 ]
+then
+echo "GOOD MORNING"
+elif [ $t -ge 12 -a $t -lt 16 ]
+then
+echo "GOOD AFTERNOON"
+elif [ $t -ge 16 -a $t -lt 20 ]
+then
+echo "GOOD EVENAING"
+else
+echo "GOOD NIGHT"
+fi
